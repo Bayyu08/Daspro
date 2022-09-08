390da9f0a2214b20c75eb6f9f3992563e1247b6a
Bayyu08 <113003476+Bayyu08@users.noreply.github.com>
2022-09-08 18:37:13 +0700

    Tugas 1

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    cout << "Bayu Triatmoko" << endl;
+    cout << "A11.2022.14621" << endl;
+    cout << "Kramat No.14,Tegal" << endl;
+    cout << "08 October 2004" << endl;
+    cout << "SMK N 1 WARUREJA" << endl;
+    cout << "Jl.Moch.Suyudi 39 Semarang Tengah" << endl;
+    return 0;
+}
