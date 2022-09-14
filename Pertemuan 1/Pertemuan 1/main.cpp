f94d9b2e87eb8ffb71239c154e57dee30434d0e2
Bayyu08 <113003476+Bayyu08@users.noreply.github.com>
2022-09-14 17:53:50 +0700

    Add files via upload

---
+++ Pertemuan 1/Pertemuan 1/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Biodata Diri" << endl;
+    cout << "Bayu Triatmoko" << endl;
+    cout << "A11.2022.14621" << endl;
+    cout << "08 Oktober 2004" << endl;
+    cout << "Suka Kamu Tapi Kamunya Ga" << endl;
+    cout << "Suka Tidur" << endl;
+    return 0;
+}
