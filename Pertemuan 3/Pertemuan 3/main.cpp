e46ee21baee700a6f33951ff6202946391d09c49
Bayyu08 <113003476+Bayyu08@users.noreply.github.com>
2022-09-14 17:40:30 +0700

    Folder
    Pengumpulan Tugas 3

---
+++ Pertemuan 3/Pertemuan 3/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    /*
+    Nama: Bayu Triatmoko
+    Nim: A11.2022.14621
+    Kelompok:4116
+    */
+
+
+    int  u = 17;
+    float t = 170.5;
+    char ub = 'L';
+    string ubs = "XL";
+
+    cout << "Umur Saya: " << u;
+    cout << endl;
+    cout << "Tinggi Saya: " << t;
+    cout << endl;
+    cout << "Ukuran Baju: " << ubs;
+    cout << endl;
+    int a;
+    cout << "\nMasukan angka: ";
+    cin >> a;
+    cout << "Angka anda + 200: " << a + u;
+
+    return 0;
+}
