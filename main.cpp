5dbd68d638f7eed4f056313558880a710e6d6fd7
Bayyu08 <113003476+Bayyu08@users.noreply.github.com>
2022-09-14 17:33:25 +0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -11,5 +11,8 @@ int main()
     cout << "08 October 2004" << endl;
     cout << "SMK N 1 WARUREJA" << endl;
     cout << "Jl.Moch.Suyudi 39 Semarang Tengah" << endl;
+    cout << "Universitas Dian Nuswantoro" << endl;
+    cout << "Suka Bermusik Dan Bermain Di Alam" << endl;
+    cout << "Hidup Saya Cuma Tidur" << endl;
     return 0;
 }
